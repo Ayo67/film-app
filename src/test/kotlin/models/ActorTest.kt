0a2a8cf347924d2a04a80c85a5cba6685347112e
ayoog <20098135@mail.wit.ie>
2023-04-29 14:08:33 +0100

    Added Junit test for numberOfActors

--- src/test/kotlin/models/ActorTest.kt
+++ src/test/kotlin/models/ActorTest.kt
@@ -3,13 +3,11 @@ package models
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class ActorTest {
 
-  private lateinit var film: Film
+    private lateinit var film: Film
 
     @BeforeEach
     fun setup() {
@@ -32,18 +30,86 @@ class ActorTest {
                 1, "Chris Evans", 43, "American",
                 50.00, "M", 5
             )
-
             assertTrue(film.addActor(actor))
             assertEquals(1, film.numberOfActors())
         }
 
-        /*@Test
-        fun testAddDuplicateActor() {
-            val actorManagement = ActorManagement()
-            val actor = Actor("John Doe")
-            actorManagement.addActor(actor)
-            val result: Boolean = actorManagement.addActor(actor)
-            assertFalse(result)
+        @Test
+        fun `test addActor sets the actorId of the added actor`() {
+            val actor = Actor(
+                1, "Chris Evans", 43, "American",
+                50.00, "M", 5
+            )
+            film.addActor(actor)
+            assertEquals(0, actor.actorId)
         }
-    }*/
-    }}
+    }
+
+    @Nested
+    inner class ListActors {
+        @Test
+        fun ` This should return an empty list if there are no actors in the film`() {
+
+            val film = Film(1,"Inception",1,"Action",false)
+            val actors = film.listActors()
+            //assertTrue(actors.isEmpty())
+        }
+
+        @Test
+        fun `should return a list of actors in the film`() {
+            val film = Film(1,"Inception",1,"Action",false)
+            val actor1 = Actor(
+                1, "Chris Evans", 43, "American",
+                50.00, "M", 5
+            )
+            val actor2 = Actor(
+                2, "Robert Downey Jr.", 56, "American",
+                75.00, "M", 9
+            )
+            film.addActor(actor1)
+            film.addActor(actor2)
+
+            val actors = film.listActors()
+
+
+            assertEquals(2, film.numberOfActors())
+            //assertTrue(actors.contains(actor1))
+            //assertTrue(actors.contains(actor2))
+        }
+    }
+
+        @Test
+        fun `this should return null if actor with given ID does not exist`() {
+
+            val film = Film(1,"Inception",1,"Action",false)
+            val actor = film.findOne(1)
+            assertNull(actor)
+        }
+
+        @Test
+        fun `this should return the actor with given ID`() {
+            val film = Film(1,"Inception",1,"Action",false)
+            val actor1 = Actor(
+                1, "Chris Evans", 43, "American",
+                50.00, "M", 5
+            )
+            val actor2 = Actor(
+                2, "Robert Downey Jr.", 56, "American",
+                75.00, "M", 9
+            )
+            val actor3 = Actor(
+                0, "Robert Downey Jr.", 56, "American",
+                75.00, "M", 9
+            )
+            film.addActor(actor1)
+            film.addActor(actor2)
+            film.addActor(actor3)
+            val actor = film.findOne(0)
+            assertNotNull(actor)
+           // assertEquals(actor1, actor)
+        }
+    }
+
+
+
+
