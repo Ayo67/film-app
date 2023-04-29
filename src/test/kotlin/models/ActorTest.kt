d5986b682fc60cd054bd7a013afc3e456d77f592
ayoog <20098135@mail.wit.ie>
2023-04-29 14:12:35 +0100

    Added Junit test for deleting an actor

--- src/test/kotlin/models/ActorTest.kt
+++ src/test/kotlin/models/ActorTest.kt
@@ -108,7 +108,33 @@ class ActorTest {
             assertNotNull(actor)
            // assertEquals(actor1, actor)
         }
+
+    @Test
+    fun `test delete removes actor with given ID`() {
+        val film = Film(1,"Inception",1,"Action",false)
+        val actor1 = Actor(
+            1, "Chris Evans", 43, "American",
+            50.00, "M", 5
+        )
+        val actor2 = Actor(
+            2, "Robert Downey Jr.", 56, "American",
+            75.00, "M", 9
+        )
+        film.addActor(actor1)
+        film.addActor(actor2)
+
+        assertTrue(film.delete(1))
+        //assertFalse(film.actors.contains(actor1))
+
+        //assertTrue(film.delete(2))
+        assertFalse(film.actors.contains(actor2))
+
+        assertFalse(film.delete(3)) // Should return false if actor with ID 3 is not in the list
     }
+
+
+
+}
 
 
 
